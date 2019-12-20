38a8c22b30323ae2641bf40a45fbf27062d59c70
Ryan Hantak <rhantak@gmail.com>
2019-12-20 09:23:49 -0700

    Add test for user search

---
+++ spec/features/user_search_for_station_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user' do
+  describe 'When I visit /' do
+    describe 'and I select Turing and click Find Nearest Station' do
+      it "I should be on the search page" do
+        expect(current_path).to eq("/search")
+      end
+
+      xit "I should see the nearest fuel station" do
+        #name
+        #address
+        #fuel type
+        #access times
+      end
+
+      xit "I should see route information to that station" do
+        #distance to station
+        #travel time
+        #directions to station
+      end
+    end
+  end
+end
