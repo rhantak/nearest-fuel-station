dc274a94d284fed55a4cb0a320fb944da3dc528d
Ryan Hantak <rhantak@gmail.com>
2019-12-20 09:37:20 -0700

    Add before each to test file

--- spec/features/user_search_for_station_spec.rb
+++ spec/features/user_search_for_station_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe 'As a user' do
   describe 'When I visit /' do
     describe 'and I select Turing and click Find Nearest Station' do
+      before(:each) do
+        visit root_path
+        click_on 'Find Nearest Station'
+      end
+
       it "I should be on the search page" do
         expect(current_path).to eq("/search")
       end
