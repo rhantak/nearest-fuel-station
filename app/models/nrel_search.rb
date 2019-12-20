5e638777dc96b783883a2d125db53290a4075cfa
Ryan Hantak <rhantak@gmail.com>
2019-12-20 10:19:59 -0700

    Add nrel search to make api request for nearest station

--- app/models/nrel_search.rb
+++ app/models/nrel_search.rb
@@ -1,14 +1,27 @@
 class NrelSearch
-  def get_nearest_station(location)
-    conn.get("/nearest.json") do |request|
+  def initialize(location)
+    @location = location
+  end
+  def get_nearest_station
+    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?") do |request|
       request.params['location'] = location
     end
+    JSON.parse(response.body)
   end
-  
+
+  def raw_location_data
+    @raw_location_data ||= get_nearest_station['fuel_stations'].first
+  end
+
+  def station
+    Station.new(raw_location_data)
+  end
+
   private
+  attr_reader :location
 
   def conn
-    Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1") do |f|
+    Faraday.new(url: "https://developer.nrel.gov") do |f|
       f.params['api_key'] = ENV['NREL_KEY']
       f.adapter Faraday.default_adapter
     end
